7c9f09511c6fa5602cefeeab467ba22a7fdaa8de
Ghenet Andemichael <38365838+Ghenet@users.noreply.github.com>
2022-04-19 09:05:02 -0700

    creates cards adds the movies retrieved

--- movie-app/script.js
+++ movie-app/script.js
@@ -4,8 +4,30 @@ const IMGPATH = "https://image.tmdb.org/t/p/w1280/"
 async function getMovies() {
     const resp = await fetch(APIURL);
     const respData = await resp.json();
+    const main = document.querySelector("main");
 
+    respData.results.forEach((movie) => {
+        const {poster_path, title, vote_average} = movie;
+
+        const movieEl = document.createElement("div");
+        movieEl.classList.add("movie");
+
+        movieEl.innerHTML = `
+            <img 
+                src="${IMGPATH + poster_path}"
+                alt="${title}"
+            />
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span>${vote_average}</span>
+            </div>`;
+
+            main.appendChild(movieEl);
+    });
+
+    return respData;
     console.log(respData);
+
 }
 
-console.log(getMovies());
+getMovies();
